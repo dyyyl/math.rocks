59184d6bd2ea8873f89d3a19262d526056d0a5b4
dyyyl <hey@dyyyl.rocks>
2020-07-12 16:49:37 +0200

    just kidding gotta fix a build

---
+++ src/pages/posts/[postSlug].tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next';
+import React from 'react';
+
+import { Title } from 'shared/components/Typography';
+
+const Post: NextPage = () => {
+  return (
+    <div>
+      <Title>A FUCKING PAGE</Title>
+    </div>
+  );
+};
+
+export default Post;

858812333e0f9ba4d375940ab2fedfb47c620da8
dyyyl <hey@dyyyl.rocks>
2020-08-16 17:44:16 +0200

    hotfix(style) katex stylesheet acting up

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -46,8 +46,8 @@ export default class Document extends NextDocument {
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css"
-            integrity="sha384-AfEj0r4/OFrOddo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X"
-            crossOrigin="anonymous"
+            // integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X"
+            // crossOrigin="anonymous"
           />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
